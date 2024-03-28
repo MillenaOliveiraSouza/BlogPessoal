80c9bd0afec0a5e030f1e4037cdf9e2f34044993
MillenaOliveiraSouza <millena10.oliveira@gmail.com>
2024-03-28 16:00:20 -0300

    Remover todos os arquivos da branch main

--- Blog_Pessoal.sql
+++
@@ -1,8 +0,0 @@
-USE db_blogpessoal;
-
-INSERT INTO tb_postagens (data, texto, titulo) 
-VALUES (current_timestamp(), "A tecnologia está em constante evolução, impulsionando mudanças e novas soluções em diversos setores", "Inovação Constante"),
-(current_timestamp(), "A tecnologia permite a conexão instantânea entre pessoas e informações ao redor do mundo, encurtando distâncias e facilitando a comunicação.", "Conectividade Global"),
-(current_timestamp(), "A transformação digital está revolucionando empresas e processos, promovendo eficiência, agilidade e novas oportunidades de negócio.", "Transformação Digital");
-
-SELECT * FROM tb_postagens;
